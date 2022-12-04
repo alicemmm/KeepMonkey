b86e5fc95d36bc59f8fb6355cd673ed393dff61a
guoyazhou.24 <guoyazhou.24@bytedance.com>
2022-12-04 14:02:44 +0800

    Initial commit

---
+++ app/src/main/java/com/asia/wutongmu/keepmonkey/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.asia.wutongmu.keepmonkey.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
